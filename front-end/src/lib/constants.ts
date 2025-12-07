8673bb124c1ff47c2b9f6025b86eb2a2873cfa38
dannick valdo <dannick.founsie@kaeyros-analytics.de>
2025-12-07 23:51:07 +0100

    fix: update Next.js to patched version

--- front-end/src/lib/constants.ts
+++ front-end/src/lib/constants.ts
@@ -4,4 +4,4 @@
 export const TOKEN_STORAGE_KEY = 'authTokens';
 
 export const API_BASE_URL =
-  `${process.env.NEXT_PUBLIC_API_URL}` || 'http://localhost:4000';
+  `${process.env.NEXT_PUBLIC_BACKEND_URL}` || 'http://localhost:4000';
